d20d6a04fab99df611f5307b7243322bea46db30
Rrahullkumar <rahulkr811844@gmail.com>
2025-05-24 20:03:03 +0530

    backend deployment step 1

--- Agri-tech-server/server.js
+++ Agri-tech-server/server.js
@@ -40,5 +40,9 @@ connectDB();
 app.use('/api/sellers', sellerRoutes); //for seller routes
 app.use('/api', stubbleRoutes);
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
